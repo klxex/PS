8a74e6b54462d6e356b7f35c9651b7bc71870bc6
klxex-admin <hwanguebest@gmail.com>
2022-08-02 21:15:14 +0900

    .

--- src/concept/Dfs.java
+++ src/concept/Dfs.java
@@ -4,7 +4,11 @@ public class Dfs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=1,T=sc.nextInt();i<=T;i++) {
+			sout
+		}
 	}
 
 }
